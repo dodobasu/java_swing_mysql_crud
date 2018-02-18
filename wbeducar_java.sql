0bca2f7105b4d254eadee19e6381044013c08c9e
Prativas Basu <35418181+dodobasu@users.noreply.github.com>
2018-02-18 08:21:41 +0530

    4TH

--- wbeducar_java.sql
+++ wbeducar_java.sql
@@ -1,27 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10.18
--- https://www.phpmyadmin.net
---
--- Host: localhost:3306
--- Generation Time: Feb 17, 2018 at 05:18 AM
--- Server version: 5.5.40-36.1-log
--- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
+SET time_zone = "+05:30";
 --
 -- Database: `wbeducar_java`
---
 
--- --------------------------------------------------------
-
+CREATE DATABASE `wbeducar_java`;
 --
 -- Table structure for table `user`
 --
@@ -41,13 +24,4 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `user_status`, `entry`) VALUES
-(1, 'tataibasu', '15081947', 'Active', '2018-02-14 22:22:09'),
-(2, 'admin', 'admin', 'Active', '2018-02-14 22:22:29'),
-(4, 'tataidsdasdasd', 'asdsadasd', 'Active', '2018-02-15 08:44:09'),
-(5, 'asdasd', 'asdasd', 'Inactive', '2018-02-16 07:16:33'),
-(7, 'goutam', '123456', 'Inactive', '2018-02-15 23:26:31'),
-(11, 'sovanghosh', '15081947', 'Active', '2018-02-16 07:11:14');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+(1, 'admin', 'admin', 'Active', '2018-02-14 22:22:29')
